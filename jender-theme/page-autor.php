4ab6a7625a79e1ca5981ab751674b0dc77fc3e90
Felipe Zapata <zapata.dev@gmail.com>
2022-12-17 17:35:09 -0500

    Fix Autor

--- jender-theme/page-autor.php
+++ jender-theme/page-autor.php
@@ -6,7 +6,7 @@
     <div class="interna-text">
       <p class="boletines-luna">HUMANIDADES</p>
       <h3 class="boletines-title">Walter Benjamin</h3>
-      <div class="interna-description interna-description_activo" id="interna-description">
+      <div class="interna-description interna-description_activo interna-description_autor" id="interna-description">
         Walter Benjamin (Berlín, 1892–Port-Bou, Gerona, 1940) es un filósofo y crítico literario alemán de origen judío. Estudió en Berlín, Friburgo y Múnich y se licenció en Berna (1918) con una importante disertación sobre el Concepto de crítica del arte en el Romanticismo alemán. Entre 1923 y 1925 trabajó en un libro sobre el Drama barroco alemán. Influido por el pensamiento de Lukács se acercó al marxismo y en el invierno de 1926 se instaló en Moscú. Volvió a Alemania, y tras abandonar Berlín a la llegada al poder del nazismo, se instaló en París. Tras la invasión de Francia fue bloqueado en la frontera española en su huida hacia Estados Unidos y, temiendo ser entregado a los alemanes, se suicidó en Port-Bou.
       </div>
       <div class="interna-compraTitulo">En Luna Libros ha publicado:</div>
