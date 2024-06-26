df748d49af81e8b6ce9f1b6eea7aaed4c1555533
Elber Castillo Mesa <elbercastillomesa@gmail.com>
2024-06-26 14:00:14 -0500

    LUN-19: Improve the single-libro feature to display similar "books posts".

--- jender-theme/page-distribucion.php
+++ jender-theme/page-distribucion.php
@@ -1,12 +1,12 @@
 <?php
-get_header();
+// Distributors Tabs Structure
+$distributors_structure = query_posts_ids_by_taxonomy('pais', array(), -1);
 
-// Pestañas
-$distributors_structure = query_posts_ids_by_taxonomy('pais', null, -1);
-
-// Lista de Distribuidores
+// Distributors List
 $query_distributors = array('ubicacion', 'rrss', 'otro');
 $distributors = query_custom_post_types($query_distributors, null, 'distribucion', 'publish', 10, 'DESC', 'post_date');
+
+get_header();
 ?>
 <main>
   <section class="distribucion">
--- jender-theme/single-libro.php
+++ jender-theme/single-libro.php
@@ -2,16 +2,38 @@
 /*
 Template Name: Single Libro
 */
+
+// Post ID
 $post_id = get_the_ID();
+$price_format = new NumberFormatter('es_CO', NumberFormatter::CURRENCY);
+
+// Books information
 $query_books = array('autor', 'cover', 'descripcion', 'coleccion', 'isbn', 'ficha_tecnica', 'pvp', 'compra_en_linea', 'prensa');
 $book = query_custom_post_types($query_books, $post_id, 'libro', 'publish', 1, 'DESC', 'post_date');
 $book = $book[0];
-$price_format = new NumberFormatter('es_CO', NumberFormatter::CURRENCY);
+
+// Recommended books
+$recommended_books = similar_book_posts($post_id, 'libro', $query_books, 'coleccion', $book['autor'], $book['coleccion'], 3);
+
+// Stores information
+$query_stores = array('url', 'logo');
+$stores = array();
+
+if (!empty($book['compra_en_linea'])) {
+  foreach ($book['compra_en_linea'] as $store) {
+    $stores[] = query_custom_post_types($query_stores, $store->ID, 'tienda', 'any', 1, 'DESC', 'post_date')[0];
+  }
+}
+
+// Collections information
+$collections = array(); 
+
+foreach ($book['coleccion'] as $coleccion) { 
+  $collections[] = $coleccion->name;  
+}
+
 get_header();
 ?>
-<pre>
-    <?php print_r($book); ?>
-</pre>
 <main>
   <section class="interna">
     <!-- TODO: responsive for the image -->
@@ -19,9 +41,10 @@ get_header();
     <div class="interna-text">
       <p class="boletines-luna">
         <?php 
-        foreach ($book['coleccion'] as $coleccion) { 
-          echo strtoupper($coleccion->name);
-          if( next( $book['coleccion'] ) ) { 
+        foreach ($collections as $collection) {
+          echo strtoupper($collection);
+          
+          if( next( $collections ) ) { 
             echo ' - ';
           }
         }      
@@ -39,36 +62,35 @@ get_header();
       <div class="interna-description" id="interna-ficha">
         <?php echo !empty($book['ficha_tecnica']) ? htmlspecialchars_decode(nl2br($book['ficha_tecnica'])) : '' ?>
       </div>
+      <?php if (!empty($book['pvp']) || !empty($book['compra_en_linea'])) { ?>
       <div class="interna-compraTitulo">Compra en línea</div>
       <div class="interna-compra">
         <!-- TODO: links? dinamicos o fijos? -->
-        <div class="interna-compra-text"><?php echo !empty($book['pvp']) ? $price_format->formatCurrency($book['pvp'], 'COP') : '' ?></div>
-        <img class="interna-compra-image" src="<?php echo get_template_directory_uri() . "/assets/imagenes/compra-logo.png;"; ?>" alt=" " />
-        <img class="interna-compra-image" src="<?php echo get_template_directory_uri() . "/assets/imagenes/compra-logo-1.png;"; ?>" alt=" " />
-        <img class="interna-compra-image" src="<?php echo get_template_directory_uri() . "/assets/imagenes/compra-logo-2.png;"; ?>" alt=" " />
-    </div>
+        <div class="interna-compra-text"><?php echo !empty($book['pvp']) ? $price_format->formatCurrency($book['pvp'], 'COP') : '' ?></div>        
+        <?php foreach ($stores as $store) { ?>
+          <a href="<?php echo $store['url']; ?>" target="_blank">
+            <img class="interna-compra-image" src="<?php echo load_default_image($store['logo']); ?>" alt="<?php echo $store['title']; ?>" />
+          </a>
+        <?php } ?>
       </div>
+      <?php } ?>
     </div>
   </section>
   <section class="interesar">
     <p class="boletines-luna">LUNA LIBROS</p>
     <h3 class="boletines-title">Te puede interesar</h3>
     <div class="interesar-principal">
+      <?php foreach ($recommended_books as $recommended_book) { ?>
       <article class="boletines-item_catalogo">
-        <img src="<?php echo get_template_directory_uri() . "/assets/imagenes/libro.png;"; ?>" alt=" " class="img-fluid radius-image boletines-images" />
-        <h4 class="boletines-item-title">Interesar</h4>
-        <p class="boletines-item-description">Esta es una descripción del libro o del artículo de máx.<br>  2 líneas.</p>
+        <a href="<?php echo $recommended_book['permalink'] ?>" class="boletines-item-link">
+          <img src="<?php echo load_default_image($recommended_book['cover']) ?>" alt=" " class="img-fluid radius-image boletines-images" />
+        </a>
+        <a href="<?php echo $recommended_book['permalink'] ?>" class="boletines-item-link">
+          <h4 class="boletines-item-title"><?php echo $recommended_book['title'] ?></h4>
+        </a>
+        <p class="boletines-item-description"><?php echo $recommended_book['descripcion'] ?></p>
       </article>
-      <article class="boletines-item_catalogo">
-        <img src="<?php echo get_template_directory_uri() . "/assets/imagenes/libro.png;"; ?>" alt=" " class="img-fluid radius-image boletines-images" />
-        <h4 class="boletines-item-title">Lecturas Lunáticas: Darío Jaramillo Agudelo</h4>
-        <p class="boletines-item-description">Esta es una descripción del libro o del artículo de máx.<br>  2 líneas.</p>
-      </article>
-      <article class="boletines-item_catalogo">
-        <img src="<?php echo get_template_directory_uri() . "/assets/imagenes/libro.png;"; ?>" alt=" " class="img-fluid radius-image boletines-images" />
-        <h4 class="boletines-item-title">Lecturas Lunáticas: Darío Jaramillo Agudelo</h4>
-        <p class="boletines-item-description">Esta es una descripción del libro o del artículo de máx.<br>  2 líneas.</p>
-      </article>
+      <?php } ?>      
     </div>
   </section>
 </main>
