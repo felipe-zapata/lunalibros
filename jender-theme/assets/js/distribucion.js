738504ff70efaa246d739c3af6bb59b2f33af8bc
Elber Castillo Mesa <elbercastillomesa@gmail.com>
2024-06-28 14:25:43 -0500

    LUN-21: Evaluate path to avoid console errors.

--- jender-theme/assets/js/distribucion.js
+++ jender-theme/assets/js/distribucion.js
@@ -1,32 +1,35 @@
 
 document.addEventListener("DOMContentLoaded", function() {
-    
-    const distribucionArreglo = document.querySelector(".distribucion-subtitulos");
-    const distribucionPaises = Array.from(distribucionArreglo.querySelectorAll("div[id^='distribucion-']"));
 
-    
-    distribucionPaises.forEach(element => {
-        element.addEventListener('click', paisesBtnClick);
-    });
+    if (window.location.pathname.includes('/distribucion/')) {
 
+        const distribucionArreglo = document.querySelector(".distribucion-subtitulos");
+        const distribucionPaises = Array.from(distribucionArreglo.querySelectorAll("div[id^='distribucion-']"));
 
-    function paisesBtnClick (event) {
-        const clickedElement = event.target;    
-    
+        
         distribucionPaises.forEach(element => {
-            element.classList.remove('catalogo-subtitulos_activo');
-        });
-        clickedElement.classList.add('catalogo-subtitulos_activo');
-               
-        // Remove the .distribucion-grid_active from all divs
-        const grids = Array.from(document.querySelectorAll('.distribucion-grid'));
-        grids.forEach(grid => {
-            grid.classList.remove('distribucion-grid_activo');
+            element.addEventListener('click', paisesBtnClick);
         });
 
-        // Add .distribucion-grid_active to the div with id #distribucion-grid-*
-        const idPais = clickedElement.id.split('-').pop(); // Get the last part after the dash
-        const gridPais = document.querySelector(`#distribucion-grid-${idPais}`);
-        gridPais.classList.add('distribucion-grid_activo');     
-      }
+
+        function paisesBtnClick (event) {
+            const clickedElement = event.target;    
+        
+            distribucionPaises.forEach(element => {
+                element.classList.remove('catalogo-subtitulos_activo');
+            });
+            clickedElement.classList.add('catalogo-subtitulos_activo');
+                
+            // Remove the .distribucion-grid_active from all divs
+            const grids = Array.from(document.querySelectorAll('.distribucion-grid'));
+            grids.forEach(grid => {
+                grid.classList.remove('distribucion-grid_activo');
+            });
+
+            // Add .distribucion-grid_active to the div with id #distribucion-grid-*
+            const idPais = clickedElement.id.split('-').pop(); // Get the last part after the dash
+            const gridPais = document.querySelector(`#distribucion-grid-${idPais}`);
+            gridPais.classList.add('distribucion-grid_activo');     
+        }
+    }
 })
--- jender-theme/assets/js/interna.js
+++ jender-theme/assets/js/interna.js
@@ -1,21 +1,27 @@
-const descriptionBtn = document.querySelector("#description-btn");
-const fichaBtn = document.querySelector("#ficha-btn");
-const internaDescription = document.querySelector("#interna-description");
-const internaFicha = document.querySelector("#interna-ficha");
+document.addEventListener("DOMContentLoaded", function() {
 
-descriptionBtn.addEventListener('click', descriptionBtnClick);
-fichaBtn.addEventListener('click', fichaBtnClick);
+  if (window.location.pathname.includes('/libro/')) {
 
-function descriptionBtnClick () {
-  internaDescription.classList.add("interna-description_activo");
-  internaFicha.classList.remove("interna-description_activo");
-  descriptionBtn.classList.add("interna-subtitulos_activo");
-  fichaBtn.classList.remove("interna-subtitulos_activo");
-}
+    const descriptionBtn = document.querySelector("#description-btn");
+    const fichaBtn = document.querySelector("#ficha-btn");
+    const internaDescription = document.querySelector("#interna-description");
+    const internaFicha = document.querySelector("#interna-ficha");
 
-function fichaBtnClick () {
-  internaDescription.classList.remove("interna-description_activo");
-  internaFicha.classList.add("interna-description_activo");
-  descriptionBtn.classList.remove("interna-subtitulos_activo");
-  fichaBtn.classList.add("interna-subtitulos_activo");
-}
+    descriptionBtn.addEventListener('click', descriptionBtnClick);
+    fichaBtn.addEventListener('click', fichaBtnClick);
+
+    function descriptionBtnClick () {
+      internaDescription.classList.add("interna-description_activo");
+      internaFicha.classList.remove("interna-description_activo");
+      descriptionBtn.classList.add("interna-subtitulos_activo");
+      fichaBtn.classList.remove("interna-subtitulos_activo");
+    }
+
+    function fichaBtnClick () {
+      internaDescription.classList.remove("interna-description_activo");
+      internaFicha.classList.add("interna-description_activo");
+      descriptionBtn.classList.remove("interna-subtitulos_activo");
+      fichaBtn.classList.add("interna-subtitulos_activo");
+    }
+  }
+})
