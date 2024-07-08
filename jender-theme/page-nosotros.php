67a617feabba254fdf154450f9c319d17a5d604f
Elber Castillo Mesa <elbercastillomesa@gmail.com>
2024-07-08 15:59:15 -0500

    LUN-24: Create the basic page for the "Nosotros" page.

--- jender-theme/page-nosotros.php
+++ jender-theme/page-nosotros.php
@@ -1,5 +1,172 @@
 <?php get_header(); ?>
 <main>
-    <!--TODO: TODA-->
+  <section>
+    <div class="boletines-header">
+      <p class="boletines-luna">LUNA LIBROS</p>
+      <h3 class="boletines-title">Quiénes Somos</h3>
+      <div class="catalogo-description">Lorem ipsum dolor sit amet consectetur adipiscing elit sodales potenti nascetur volutpat tortor, metus tempus molestie habitant nisi penatibus lacus ultrices non velit etiam lobortis parturient, aliquet rutrum facilisis cubilia porta nec sagittis eget massa varius habitasse.</div>      
+    </div>
+  </section>
+  <section class="post">
+    <div class="post nosotros-section">
+        <p class="nosotros-tag">LUNA LIBROS</p>
+        <h4 class="nosotros-subtitle">Nuestro Objetivo</h4>
+        <p class="nosotros-paragraph">
+            Construiremos un catálogo que nos permita leer América desde América y aportar conocimientos sobre el continente, 
+            publicar textos útiles para estudiantes y docentes universitarios y lectores interesados en las ciencias sociales y 
+            humanas y presentar autores y visiones que contribuyen a poner en valor la palabra, pues el gran capital común del 
+            continente americano es nuestra lengua
+        </p>
+    </div>
+    <div class="post nosotros-section">
+        <p class="nosotros-tag">LUNA LIBROS</p>
+        <h4 class="nosotros-subtitle">Decálogo del Lector lunático</h4>
+        <div class="nosotros-grid">
+
+            <div class="flip-container">
+                <div class="card">
+                    <div class="front">
+                        <img src="<?php echo get_template_directory_uri() . "/assets/imagenes/libro.png;"; ?>" alt=" " class="img-fluid radius-image nosotros-image" />
+                    </div>
+                    <div class="back">
+                        <h4>1.</h4>
+                        <p>Lecturas Lunaticas Decalogo #1</p>
+                    </div>
+                </div>
+            </div>
+            <div class="flip-container">
+                <div class="card">
+                    <div class="front">
+                        <img src="<?php echo get_template_directory_uri() . "/assets/imagenes/libro.png;"; ?>" alt=" " class="img-fluid radius-image nosotros-image" />
+                    </div>
+                    <div class="back">
+                        <h4>1.</h4>
+                        <p>Lecturas Lunaticas Decalogo #1</p>
+                    </div>
+                </div>
+            </div>
+            <div class="flip-container">
+                <div class="card">
+                    <div class="front">
+                        <img src="<?php echo get_template_directory_uri() . "/assets/imagenes/libro.png;"; ?>" alt=" " class="img-fluid radius-image nosotros-image" />
+                    </div>
+                    <div class="back">
+                        <h4>1.</h4>
+                        <p>Lecturas Lunaticas Decalogo #1</p>
+                    </div>
+                </div>
+            </div>
+            <div class="flip-container">
+                <div class="card">
+                    <div class="front">
+                        <img src="<?php echo get_template_directory_uri() . "/assets/imagenes/libro.png;"; ?>" alt=" " class="img-fluid radius-image nosotros-image" />
+                    </div>
+                    <div class="back">
+                        <h4>1.</h4>
+                        <p>Lecturas Lunaticas Decalogo #1</p>
+                    </div>
+                </div>
+            </div>
+            <div class="flip-container">
+                <div class="card">
+                    <div class="front">
+                        <img src="<?php echo get_template_directory_uri() . "/assets/imagenes/libro.png;"; ?>" alt=" " class="img-fluid radius-image nosotros-image" />
+                    </div>
+                    <div class="back">
+                        <h4>1.</h4>
+                        <p>Lecturas Lunaticas Decalogo #1</p>
+                    </div>
+                </div>
+            </div>
+            <div class="flip-container">
+                <div class="card">
+                    <div class="front">
+                        <img src="<?php echo get_template_directory_uri() . "/assets/imagenes/libro.png;"; ?>" alt=" " class="img-fluid radius-image nosotros-image" />
+                    </div>
+                    <div class="back">
+                        <h4>1.</h4>
+                        <p>Lecturas Lunaticas Decalogo #1</p>
+                    </div>
+                </div>
+            </div>
+            <div class="flip-container">
+                <div class="card">
+                    <div class="front">
+                        <img src="<?php echo get_template_directory_uri() . "/assets/imagenes/libro.png;"; ?>" alt=" " class="img-fluid radius-image nosotros-image" />
+                    </div>
+                    <div class="back">
+                        <h4>1.</h4>
+                        <p>Lecturas Lunaticas Decalogo #1</p>
+                    </div>
+                </div>
+            </div>
+            <div class="flip-container">
+                <div class="card">
+                    <div class="front">
+                        <img src="<?php echo get_template_directory_uri() . "/assets/imagenes/libro.png;"; ?>" alt=" " class="img-fluid radius-image nosotros-image" />
+                    </div>
+                    <div class="back">
+                        <h4>1.</h4>
+                        <p>Lecturas Lunaticas Decalogo #1</p>
+                    </div>
+                </div>
+            </div>
+            <div class="flip-container">
+                <div class="card">
+                    <div class="front">
+                        <img src="<?php echo get_template_directory_uri() . "/assets/imagenes/libro.png;"; ?>" alt=" " class="img-fluid radius-image nosotros-image" />
+                    </div>
+                    <div class="back">
+                        <h4>1.</h4>
+                        <p>Lecturas Lunaticas Decalogo #1</p>
+                    </div>
+                </div>
+            </div>
+            <div class="flip-container">
+                <div class="card">
+                    <div class="front">
+                        <img src="<?php echo get_template_directory_uri() . "/assets/imagenes/libro.png;"; ?>" alt=" " class="img-fluid radius-image nosotros-image" />
+                    </div>
+                    <div class="back">
+                        <h4>1.</h4>
+                        <p>Lecturas Lunaticas Decalogo #1</p>
+                    </div>
+                </div>
+            </div>
+        </div>
+    </div>
+    <div class="post nosotros-section">
+        <p class="nosotros-tag">LUNA LIBROS</p>
+        <h4 class="nosotros-subtitle">Servicios editorialies</h4>
+        <p class="nosotros-paragraph">
+            Construiremos un catálogo que nos permita leer América desde América y aportar conocimientos sobre el continente, 
+            publicar textos útiles para estudiantes y docentes universitarios y lectores interesados en las ciencias sociales y 
+            humanas y presentar autores y visiones que contribuyen a poner en valor la palabra, pues el gran capital común del 
+            continente americano es nuestra lengua
+        </p>
+    </div>
+    <div class="post nosotros-section">
+        <p class="nosotros-tag">LUNA LIBROS</p>
+        <h4 class="nosotros-subtitle">Preguntas frecuentes</h4>
+        <div class="qa-container">
+            <button class="qa-button"></button>
+            <div class="qa-question">                
+                ¿Cuál es el tiempo de entrega de los libros?
+            </div>
+            <div class="qa-answer">
+                Si estás interesado en invitar a la editorial o a alguno de nuestros autores lunáticos, construiremos un catálogo que nos permita leer América desde América y aportar conocimientos sobre el continente, publicar textos útiles para estudiantes y docentes universitarios y lectores interesados en las ciencias sociales y humanas y presentar autores y visiones que contribuyen a poner en valor la palabra, pues el gran capital común del continente americano es nuestra lengua.
+            </div>
+        </div>
+        <div class="qa-container">
+            <button class="qa-button"></button>    
+            <div class="qa-question">
+                ¿Cuántos libros hay en el catálogo?
+            </div>
+            <div class="qa-answer">
+                Si estás interesado en invitar a la editorial o a alguno de nuestros autores lunáticos, construiremos un catálogo que nos permita leer América desde América y aportar conocimientos sobre el continente, publicar textos útiles para estudiantes y docentes universitarios y lectores interesados en las ciencias sociales y humanas y presentar autores y visiones que contribuyen a poner en valor la palabra, pues el gran capital común del continente americano es nuestra lengua.
+            </div>
+        </div>
+    </div>
+  </section>
 </main>
 <?php get_footer(); ?>
